e3c93b0cbc94a067506545666f2bd1c81a3846d4
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2022-01-02 17:49:33 +0530

    2nd Question completed

---
+++ q1.js
@@ -0,0 +1,6 @@
+//create function for count the number of items in an array 
+function item_count(arr){
+    console.log("item count: "+arr.length);
+}
+//function call with as array parameter
+item_count([10,20,30,40,50]);
