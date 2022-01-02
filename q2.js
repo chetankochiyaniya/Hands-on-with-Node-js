56619f7733fbd0334ec86c851262c94673c6aac5
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2022-01-02 17:50:26 +0530

    3rd Question completed

---
+++ q2.js
@@ -0,0 +1,7 @@
+var name = "Chetan Kochiyaniya";
+var res = (s1,s2,s3)=>{
+    return (s1+s2+s3)/3;
+}
+var s = ['ui','android','python'];
+
+module.exports={name,res,s};
---
+++ q3.js
@@ -0,0 +1,2 @@
+var obj = require('./q2');
+console.log("name: "+obj.name+"\ncgpa: "+obj.res(40,30,50)+"\nsubject name: "+obj.s );
